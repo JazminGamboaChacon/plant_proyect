4c0c660c4d35765dd64666dcf7db47f77ca8ca6c
JazminGamboaChacon <154479555+JazminGamboaChacon@users.noreply.github.com>
2026-03-04 20:19:08 -0600

    refactor: replace useProfileTheme with useTheme for improved styling in CustumSafeAreaView

--- src/layout/CustumSafeAreaView.tsx
+++ src/layout/CustumSafeAreaView.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { useProfileTheme } from "./CustumSafeArea.style";
+import { useTheme } from "../context/ThemeContext";
 
 export default function CustumSafeAreaView({
   children,
 }: {
   children: React.ReactNode | React.ReactNode[];
 }) {
-  const { styles } = useProfileTheme();
-  return <SafeAreaView style={styles.container}>{children}</SafeAreaView>;
+  const { theme } = useTheme();
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: theme.colors.bg }}>
+      {children}
+    </SafeAreaView>
+  );
 }
