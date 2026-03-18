3423bdcd80e504318f33e4e00fe4dbd20be0ae14
JazminGamboaChacon <154479555+JazminGamboaChacon@users.noreply.github.com>
2026-03-18 15:22:35 -0600

    feat: add index screen to RootLayout for improved navigation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -21,6 +21,7 @@ export default function RootLayout() {
   return (
     <ThemeProvider>
       <Stack>
+        <Stack.Screen name="index" options={{ headerShown: false }} />
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
       </Stack>
     </ThemeProvider>
