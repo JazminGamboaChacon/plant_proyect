d36b1385f0a1254535039b87d339214a655d792c
JazminGamboaChacon <154479555+JazminGamboaChacon@users.noreply.github.com>
2026-03-18 15:49:15 -0600

    feat: add register screen to RootLayout for improved navigation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -22,6 +22,7 @@ export default function RootLayout() {
     <ThemeProvider>
       <Stack>
         <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen name="register" options={{ headerShown: false }} />
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
       </Stack>
     </ThemeProvider>
