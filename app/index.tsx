49c524b5406e38f274b28625de83e397a239863e
JazminGamboaChacon <154479555+JazminGamboaChacon@users.noreply.github.com>
2026-03-27 12:56:39 -0600

    feat: implement authentication flow in Index component with loading state and redirection

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,22 @@
-//import UserProfile from "@/src/screens/userProfile/UserProfile";
-//export default function Index() {
-//  return <UserProfile />;
-//}
-import LoginScreen from "@/src/screens/Login/LoginScreen";
+import { Redirect } from "expo-router";
+import { ActivityIndicator, View } from "react-native";
+import { useAuth } from "../src/context/AuthContext";
+import LoginScreen from "../src/screens/Login/LoginScreen";
 
 export default function Index() {
+  const { user, isLoading } = useAuth();
+
+  if (isLoading) {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <ActivityIndicator size="large" />
+      </View>
+    );
+  }
+
+  if (user) {
+    return <Redirect href="/(tabs)" />;
+  }
+
   return <LoginScreen />;
 }
