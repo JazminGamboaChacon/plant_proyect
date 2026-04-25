09570c31b75237d020d9a4147fa4c685e95056e2
JazminGamboaChacon <154479555+JazminGamboaChacon@users.noreply.github.com>
2026-04-24 18:05:34 -0600

    feat: add plant-detail screen to RootLayout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -32,6 +32,7 @@ export default function RootLayout() {
           <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
           <Stack.Screen name="edit-profile" options={{ headerShown: false }} />
           <Stack.Screen name="edit-plant" options={{ headerShown: false }} />
+          <Stack.Screen name="plant-detail" options={{ headerShown: false }} />
         </Stack>
         </SyncProvider>
       </ToastProvider>
