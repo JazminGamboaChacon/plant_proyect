68574e449058c9d838624a71b795a8c4600721b0
JazminGamboaChacon <154479555+JazminGamboaChacon@users.noreply.github.com>
2026-05-16 22:37:46 -0600

    feat: wrap Stack component with RegistrationProvider for user registration context

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -7,6 +7,7 @@ import {
 import { Lora_400Regular_Italic } from "@expo-google-fonts/lora";
 import { Stack } from "expo-router";
 import { AuthProvider } from "../src/context/AuthContext";
+import { RegistrationProvider } from "../src/context/RegistrationContext";
 import { ThemeProvider } from "../src/context/ThemeContext";
 
 export default function RootLayout() {
@@ -22,16 +23,18 @@ export default function RootLayout() {
   return (
     <ThemeProvider>
       <AuthProvider>
-        <Stack>
-          <Stack.Screen name="index" options={{ headerShown: false }} />
-          <Stack.Screen name="register" options={{ headerShown: false }} />
-          <Stack.Screen
-            name="profile-setup"
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen name="preference" options={{ headerShown: false }} />
-          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        </Stack>
+        <RegistrationProvider>
+          <Stack>
+            <Stack.Screen name="index" options={{ headerShown: false }} />
+            <Stack.Screen name="register" options={{ headerShown: false }} />
+            <Stack.Screen name="profile-setup" options={{ headerShown: false }} />
+            <Stack.Screen name="preference" options={{ headerShown: false }} />
+            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+            <Stack.Screen name="edit-profile" options={{ headerShown: false }} />
+            <Stack.Screen name="edit-plant" options={{ headerShown: false }} />
+            <Stack.Screen name="plant-detail" options={{ headerShown: false }} />
+          </Stack>
+        </RegistrationProvider>
       </AuthProvider>
     </ThemeProvider>
   );
